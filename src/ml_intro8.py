0d962fa8627d12378e832acec2cbf866b0fe6dfa
Alex Morehead <alex.morehead@gmail.com>
2018-05-12 20:12:26 -0500

    Finished up k-nearest neighbor examples.

--- src/ml_intro8.py
+++ src/ml_intro8.py
@@ -2,24 +2,30 @@ import numpy as np
 from sklearn import model_selection, neighbors
 import pandas as pd
 
-df = pd.read_csv('breast-cancer-wisconsin.data')
-df.replace('?', -99999, inplace=True)
-df.drop(['id'], 1, inplace=True)
+accuracies = []
 
-X = np.array(df.drop(['class'], 1))
-y = np.array(df['class'])
+for i in range(25):
+    df = pd.read_csv('breast-cancer-wisconsin.data')
+    df.replace('?', -99999, inplace=True)
+    df.drop(['id'], 1, inplace=True)
 
-X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+    X = np.array(df.drop(['class'], 1))
+    y = np.array(df['class'])
 
-clf = neighbors.KNeighborsClassifier()
-clf.fit(X_train, y_train)
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
-accuracy = clf.score(X_test, y_test)
-print(accuracy)
+    clf = neighbors.KNeighborsClassifier(n_jobs=1)
+    clf.fit(X_train, y_train)
 
-example_measures = np.array([[4, 2, 1, 1, 1, 2, 3, 2, 1], [4, 2, 1, 2, 2, 2, 3, 2, 1], [3, 4, 2, 3, 4, 5, 6, 2, 3]])
+    accuracy = clf.score(X_test, y_test)
+    ##    #print(accuracy)
+    ##
+    ##    example_measures = np.array([[4, 2, 1, 1, 1, 2, 3, 2, 1], [4, 2, 1, 2, 2, 2, 3, 2, 1], [3, 4, 2, 3, 4, 5, 6, 2, 3]])
+    ##
+    ##    example_measures = example_measures.reshape(len(example_measures), -1)
+    ##
+    ##    prediction = clf.predict(example_measures)
+    ##    #print(prediction)
+    accuracies.append(accuracy)
 
-example_measures = example_measures.reshape(len(example_measures), -1)
-
-prediction = clf.predict(example_measures)
-print(prediction)
+print(sum(accuracies) / len(accuracies))
