c5b8f6edb49d78f93380175d0949303604583f46
Alex Morehead <alex.morehead@gmail.com>
2018-05-20 00:32:56 -0500

    Added a script to introduce Tensorflow for neural networks.

---
+++ src/ml_fun23.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+x1 = tf.constant(5)
+x2 = tf.constant(6)
+# result = x1 * x2
+result = tf.multiply(x1, x2)
+print(result)
+
+with tf.Session() as sess:
+    output = sess.run(result)
+    print(output)
+
+print(output)
