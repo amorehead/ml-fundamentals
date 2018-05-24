f8d7db1a5431f514efc306f11233a2b98d26074e
Alex Morehead <alex.morehead@gmail.com>
2018-05-23 23:24:27 -0500

    Began implementing an experiment with OpenAI's gym feature.

---
+++ src/ml_fun33.py
@@ -0,0 +1,29 @@
+import gym
+import random
+import numpy as np
+import tflearn
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+from statistics import mean, median
+from collections import Counter
+
+LR = 1e-3
+env = gym.make("CartPole-v1")
+env.reset()
+goal_steps = 500
+score_requirements = 50
+initial_games = 10000
+
+
+def some_random_games():
+    for episode in range(5):
+        env.reset()
+        for t in range(goal_steps):
+            env.render()
+            action = env.action_space.sample()
+            observation, reward, done, info = env.step(action)
+            if done:
+                break
+
+
+some_random_games()
